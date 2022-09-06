1947c2b5d9b02c414e85daa698b04c78b623a491
Jesse Ruhland <jesse.ruhland@gmail.com>
2022-09-06 15:24:53 -0400

    Part 3 completed

--- queries_playstore.sql
+++ queries_playstore.sql
@@ -1,1 +1,61 @@
 -- Comments in SQL Start with dash-dash --
+
+-- QUESTION 1:
+-- 
+-- ANSWER:  
+
+-- QUESTION 2:
+-- 
+-- ANSWER:  
+
+-- QUESTION 3:
+-- 
+-- ANSWER:  
+
+-- QUESTION 4:
+-- 
+-- ANSWER:  
+
+-- QUESTION 5:
+-- 
+-- ANSWER:  
+
+-- QUESTION 6:
+-- 
+-- ANSWER:  
+
+-- QUESTION 7:
+-- 
+-- ANSWER:  
+
+-- QUESTION 8:
+-- 
+-- ANSWER:  
+
+-- QUESTION 9:
+-- 
+-- ANSWER:  
+
+-- QUESTION 10:
+-- 
+-- ANSWER:  
+
+-- QUESTION 11:
+-- 
+-- ANSWER:  
+
+-- QUESTION 12:
+-- 
+-- ANSWER:  
+
+-- QUESTION 13:
+-- 
+-- ANSWER:  
+
+-- QUESTION 14:
+-- 
+-- ANSWER:  
+
+-- QUESTION 15:
+--
+-- ANSWER:  
--- queries_products.sql
+++ queries_products.sql
@@ -1,1 +1,57 @@
 -- Comments in SQL Start with dash-dash --
+
+-- QUESTION 1:
+-- Add a product to the table with the name of “chair”, price of 44.00, and can_be_returned of false.
+-- ANSWER:  INSERT INTO products (name, price, can_be_returned) VALUES ('chair', 44.00, False);
+
+-- QUESTION 2:
+-- Add a product to the table with the name of “stool”, price of 25.99, and can_be_returned of true.
+-- ANSWER:  INSERT INTO products (name, price, can_be_returned) VALUES ('stool', 25.99, True);
+
+-- QUESTION 3:
+-- Add a product to the table with the name of “table”, price of 124.00, and can_be_returned of false.
+-- ANSWER:  INSERT INTO products (name, price, can_be_returned) VALUES ('table', 124.00, False);
+
+-- QUESTION 4:
+-- Display all of the rows and columns in the table.
+-- ANSWER:  SELECT * FROM products;
+
+-- QUESTION 5:
+-- Display all of the names of the products.
+-- ANSWER:  SELECT name FROM products;
+
+-- QUESTION 6:
+-- Display all of the names and prices of the products.
+-- ANSWER:  SELECT name, price FROM products;
+
+-- QUESTION 7:
+-- Add a new product - make up whatever you would like!
+-- ANSWER:  INSERT INTO products (name, price, can_be_returned) VALUES ('couch', 317.00, False);
+
+-- QUESTION 8:
+-- Display only the products that can_be_returned.
+-- ANSWER:  SELECT * FROM products WHERE can_be_returned = True;
+
+-- QUESTION 9:
+-- Display only the products that have a price less than 44.00.
+-- ANSWER:  SELECT * FROM products WHERE price < 44.00;
+
+-- QUESTION 10:
+-- Display only the products that have a price in between 22.50 and 99.99.
+-- ANSWER:  SELECT * FROM products WHERE price > 22.50 AND price < 99.99;
+
+-- QUESTION 11:
+-- There’s a sale going on: Everything is $20 off! Update the database accordingly.
+-- ANSWER:  UPDATE products SET price = (price - 20.00);
+
+-- QUESTION 12:
+-- Because of the sale, everything that costs less than $25 has sold out. Remove all products whose price meets this criteria.
+-- ANSWER:  DELETE FROM products WHERE price < 25.00;
+
+-- QUESTION 13:
+-- And now the sale is over. For the remaining products, increase their price by $20.
+-- ANSWER:  UPDATE products SET price = (price + 20.00);
+
+-- QUESTION 14:
+-- There is a new company policy: everything is returnable. Update the database accordingly.
+-- ANSWER:  UPDATE products SET can_be_returned = True;
